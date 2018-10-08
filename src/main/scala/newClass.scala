71a4c027154241fe39649e13f5b12fd4a0eeb9c9
Nekesa Mercy <nekesamercy@dhcp205-071-123.protected.buffalo.edu>
2018-10-08 11:01:08 -0400

    defining test methods

--- src/main/scala/newClass.scala
+++ src/main/scala/newClass.scala
@@ -5,59 +5,84 @@ object newClass {
 
   // declare a var to for the user input
 
-  var prompt = "Please enter the string to which you 555 would like to find the data type trial";
+  var prompt = "Please enter the string to which you 555 would like to find the data type trial"
 
-  val numberPattern: Regex = "[0-9]".r
+    val numberPattern: Regex = "[0-9]".r
 
+    val address = "123 Main Street Suite 101"
 
-
-
-
-    numberPattern.findFirstMatchIn(prompt) match {
-
-    case Some(_) => println("Password ok")
-    case None => println ("Passowrd must contain a number")
-
-  }
-
-
-  //read string from a file
-
-//  if (args.length > 0) {
-//
-//    for (line <- Source.fromFile(args(0)).getLines())
-//
-//      println(line.length + " " + line)
-//
-//  }
-//  else
-//    Console.err.println("Please enter a file name")
-
-
-  //detect data type methods
-  // detect a data type of int in the prompt
-
-  def max(x: Int, y: Int): Int = {
-
-
-    if (x > y) x
-
-    else y
-  }
-
-
-  def main(args: Array[String]): Unit = {
+    val match1 = numberPattern.findFirstIn(address)
 
 
 
 
-    println(max(3,5))
 
-    println(prompt)
-
+  //detect int
+  def findInt ( ): Unit = {
 
 
 
   }
+
+
+
+  //detect float
+
+  def findFloat ( ): Unit = {
+
+
+  }
+
+
+
+  // detect date
+
+
+  def findDate ( ): Unit ={
+
+
+  }
+
+
+
+  //detect boolean
+
+  def findBool ( ): Unit ={
+
+
+  }
+
+
+
+  //detect time stamp
+
+   def findTime ( ): Unit = {
+
+
+   }
+
+
+
+def max(x: Int, y: Int): Int = {
+
+
+  if (x > y) x
+
+  else y
+}
+
+
+def main(args: Array[String]): Unit = {
+
+
+  println(max(3, 5))
+
+  println(prompt)
 
 }
+
+
+}
+
+
+
