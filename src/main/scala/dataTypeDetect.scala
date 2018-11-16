1645caa955ae10993735028a485889f8fc6be4cb
Oliver Kennedy <okennedy.gitkr@xthemage.net>
2018-11-16 13:42:34 -0500

    Working through for loops

--- src/main/scala/dataTypeDetect.scala
+++ src/main/scala/dataTypeDetect.scala
@@ -10,22 +10,29 @@ object dataTypeDetect {
 
   //regex to check data types
 
-  var chekXpression = Map[Regex] = Seq(
-
-  var numIntegers= findType("[0-9]".r, ts.dataSeq),
-  var numfloat = findType("[+-]?([0-9]*[.])?[0-9]+".r, ts.dataSeq),
-  var numDates = findType("""(0?[1-9]|1[012])[- \/.](0?[1-9]|[12][0-9]|3[01])[- \/.](19|20)\d\d""".r, ts.dataSeq),
-  var numBoolean = findType("([Vv]+(erdade(iro)?)?|[Ff]+(als[eo])?|[Tt]+(rue)?|0|[\\+\\-]?1)".r, ts.dataSeq),
-  var numTimeStamps = findType("\\b([0-1][0-9]|[2][0-3]):([0-5][0-9])\\b".r, ts.dataSeq)
-
-
+  var chekXpression: Map[String,Regex] = Map(
+    "Integer"    -> "[0-9]".r, 
+    "Float"      -> "[+-]?([0-9]*[.])?[0-9]+".r, 
+    "Date"       -> """(0?[1-9]|1[012])[- \/.](0?[1-9]|[12][0-9]|3[01])[- \/.](19|20)\d\d""".r, 
+    "Boolean"    -> "([Vv]+(erdade(iro)?)?|[Ff]+(als[eo])?|[Tt]+(rue)?|0|[\\+\\-]?1)".r, 
+    "TimeStamp"  -> "\\b([0-1][0-9]|[2][0-3]):([0-5][0-9])\\b".r, 
   )
 
+  def findAllTypes(data: Seq[String]): Map[String, Int] = {
+
+    for( (typeName:String, regex:Regex) <- chekXpression ) {
+      println(typeName)
+      val count = findType(regex, data)
+      println("Found "+count+" "+typeName+"(s)")
+    }
+
+    ???
+  }
 
   //method to determine the dataTypes
-  def findType (regularXp: Regex , fileName: Seq[String]): Int = {
+  def findType (regularXp: Regex , data: Seq[String]): Int = {
 
-    var numInt: MatchIterator = regularXp.findAllIn(fileName.mkString("\n"))
+    var numInt: MatchIterator = regularXp.findAllIn(data.mkString("\n"))
 
     return numInt.size
   }
@@ -34,12 +41,12 @@ object dataTypeDetect {
 def main(args: Array[String]): Unit = {
 
 // print the results
-
-  println("this is the number of integers in the string " + numIntegers);
-  println("this is the number of float numbers " + numfloat);
-  println("this is the number of dates in the string " + numDates);
-  println( "this is the number of boolean in the string " + numBoolean);
-  println( "this is the number of time stamps in the string " + numTimeStamps);
+  findAllTypes(ts.dataSeq)
+  // println("this is the number of integers in the string " + numIntegers);
+  // println("this is the number of float numbers " + numfloat);
+  // println("this is the number of dates in the string " + numDates);
+  // println( "this is the number of boolean in the string " + numBoolean);
+  // println( "this is the number of time stamps in the string " + numTimeStamps);
 
 
 }
