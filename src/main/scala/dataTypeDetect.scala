26196d4221d8daaab7c3117b40d536f19a172fdb
Oliver Kennedy <okennedy.gitkr@xthemage.net>
2018-12-03 13:25:39 -0500

    Moving code to test case

--- src/main/scala/dataTypeDetect.scala
+++ src/main/scala/dataTypeDetect.scala
@@ -1,35 +1,8 @@
 
 import scala.util.matching.Regex
 import scala.util.matching.Regex.MatchIterator
-import scala.io.Source._
 
 object dataTypeDetect {
-  //create object for testCases class
-  //val ts = new testCases( )
-
-  val data = fromFile("/Users/nekesamercy/IdeaProjects/typeDetect/src/main/scala/zs.csv").getLines
-
-  //val data = Seq("A,B,C","1,3,4","2,,","1,2,6","1,4,6","2,2,6",",2,6") //fromFile("zs.csv").getLines
-
-  val rowData = data.map(row => {
-    (if(row.trim.startsWith(","))
-      s"***${row}"
-    else if(row.endsWith(","))
-      s"${row}***"
-    else
-      row).split(",").toSeq.map(el => if(el.equals("***")) "" else el)
-  })
-
-  val columnarData = rowData.foldLeft(Seq[Seq[String]]())((init, cur) => {
-    init match {
-      case Seq() => cur.map(cell => Seq(cell))
-      case nonEmptySeq => cur.zipWithIndex.map(cellidx => nonEmptySeq(cellidx._2) :+ s"${cellidx._1}")
-      case _ => init
-    }
-  })
-
-
-  columnarData.mkString("<br>")
 
 
   //regex to check data types
@@ -65,17 +38,6 @@ object dataTypeDetect {
 
     return numInt.size
   }
-
-
-  def main(args: Array[String]): Unit = {
-
-    // print the results
-    columnarData.tail.map(col => findAllTypes(col))
-
-
-
-  }
-
 
 }
 
